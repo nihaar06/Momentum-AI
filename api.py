dff389994316c61be221ec3b5e8c917bac71ab3d
Nihaar G <nihaar206@gmail.com>
2026-01-25 15:23:25 +0530

    Updated api.py

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://your-frontend.vercel.app"
+        "https://momentum-ai-frontend.vercel.app",
     ],  
     allow_credentials=True,
     allow_methods=["*"],
